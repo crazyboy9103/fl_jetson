2a718a7cbf7233954562bfc97750615fbf39109b
crazyboy9103 <gil9103@gmail.com>
2022-03-22 14:45:59 +0900

    from config

--- socket/jetson_fl.py
+++ socket/jetson_fl.py
@@ -49,8 +49,8 @@ class FLClient:
             #import threading
             #lock = threading.Lock()
             #model = tf.keras.models.model_from_json(model, custom_objects={"null":None}) 
-
-            self.model = tf.keras.models.model_from_config(model_arch) #, custom_objects={"null":None})
+    
+            self.model = tf.keras.Sequential().from_config(model_arch) #, custom_objects={"null":None})
 
             #print("model arch", model_arch)
 
